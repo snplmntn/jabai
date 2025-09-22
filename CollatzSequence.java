0c6623e264fbeedf38820f9331b06237c9e11b01
Mark John Paul M. Monton <34324883+markjohn519@users.noreply.github.com>
2025-09-22 20:58:58 +0800

    cleanup code comments

--- CollatzSequence.java
+++ CollatzSequence.java
@@ -1,32 +1,3 @@
-
-// Pseudocode:
-// 1. Set a starting number
-// 2. Initialize step counter to 0
-// 3. While number is not 1:
-//      a. If number is even, divide by 2
-//      b. If number is odd, multiply by 3 and add 1
-//      c. Increment step counter
-// 4. Output the total steps
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 public class CollatzSequence {
     public static void main(String[] args) {
         // 1. Set a starting number
@@ -35,17 +6,16 @@ public class CollatzSequence {
         int steps = 0;
 
         // 3. While number is not 1:
-        while (number != 1) {  // Loop until number reaches 1
+        while (number != 1) {
             // a. If number is even, divide by 2
-            if (number % 2 == 0) { // If even
+            if (number % 2 == 0) {
                 number = number / 2;
             // b. If number is odd, multiply by 3 and add 1
-            } else { // If odd
+            } else {
                 number = number * 3 + 1;
             }
             // c. Increment step counter
-            steps++; // Increment step counter
-            // para kay sean
+            steps++;
         }
 
         //  4. Output the total steps
