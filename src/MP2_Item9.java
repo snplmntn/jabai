8a435e1ab55c1230ca74a38a505ed5cf326ac3f2
adi <bantogsalahadin@gmail.com>
2025-10-06 19:59:05 +0800

    b-b-bus!

--- src/MP2_Item9.java
+++ src/MP2_Item9.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
 
 public class MP2_Item9 {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("---GREATEST COMMON DIVISOR---\n");
+
+
+        System.out.print("Enter the size of your numbers: ");
+        int arraySize = scanner.nextInt();
+
+        int[] array = new int[arraySize];
+
+        System.out.println("Enter your numbers:");
+        for (int index = 0; index < arraySize; index ++){
+            System.out.printf("Number %d: ", index + 1);
+            array[index] = scanner.nextInt();
+        }
+
+        int result = lcdArray(array);
+
+        System.out.println("The LCM of the given numbers are: " + result);
+
+
+        scanner.close();
+    }
+
+    public static int lcm(int first, int second){
+        return (first * second) / gcd(first, second);
+    }
+
+    public static int lcdArray(int[] numbers){
+        int result = numbers[0];
+        for (int index = 1; index < numbers.length; index ++){
+            result = lcm(result, numbers[index]);
+        }
+        return result;
+    }
+
+
+    public static int gcd(int first, int second){
+        while (second != 0){
+            int temp = second;
+            second = first % second;
+            first = temp;
+
+        }
+        return first;
+    }
 }
