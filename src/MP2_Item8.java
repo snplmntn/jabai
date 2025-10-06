39ee7e1a9fd12c1103c29438e8010f7e0c954a32
adi <bantogsalahadin@gmail.com>
2025-10-06 19:57:15 +0800

    luh

--- src/MP2_Item8.java
+++ src/MP2_Item8.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
 
 public class MP2_Item8 {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("---GREATEST COMMON DIVISOR---\n");
+
+        System.out.print("Enter the size of your numbers: ");
+        int arraySize = scanner.nextInt();
+
+        int[] array = new int[arraySize];
+
+        System.out.println("Enter your numbers:");
+        for (int index = 0; index < arraySize; index ++){
+            System.out.printf("Number %d: ", index + 1);
+            array[index] = scanner.nextInt();
+        }
+
+        int result = gcdArray(array);
+
+        System.out.println("The GCD of the given numbers are: " + result);
+
+
+        scanner.close();
+    }
+
+    public static int gcd(int first, int second){
+        while (second != 0){
+            int temp = second;
+            second = first % second;
+            first = temp;
+
+        }
+        return first;
+    }
+
+
+    public static int gcdArray(int numbers[]){
+        int result = numbers[0];
+
+        for (int index = 0; index < numbers.length; index ++){
+            result = gcd(result, numbers[index]);
+        }
+        return result;
+    }
 }
